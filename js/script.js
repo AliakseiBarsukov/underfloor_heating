3834b7983256863933ee8f5ff29c42e02f738e2b
Aliaksei Barsukov <barsecky@gmail.com>
2023-04-19 17:26:26 +0300

    ended responsive version

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Swiper slider
+
+new Swiper('.hero__slider', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: true,
+
+  navigation: {
+    prevEl: '.hero__slider-btn_prev',
+    nextEl: '.hero__slider-btn_next',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    560: {
+      spaceBetween: 8,
+    },
+  },
+});
+
+
+// Calculator
+
+
